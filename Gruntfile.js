932ff84fbb0bfa4a6889bda5eb70e5256f0bd5a7
chenyong <yong.chen@doone.com.cn>
2019-12-08 23:37:34 +0800

    细节修改

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,23 @@
 module.exports = function(grunt){
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  }
   grunt.initConfig({
+    config: config,
+    copy: {
+      dist: {
+        src: '<%= config.app %>/index.html',
+        dest: '<%= config.dist %>/index.html'
+      }
+    },
+    clean: {
+      dist: {
+        src: '<%= config.dist%>/index.html'
+      }
+    },
     pkg: grunt.file.readJSON('package.json'),
     ugligy: {
       options: {
